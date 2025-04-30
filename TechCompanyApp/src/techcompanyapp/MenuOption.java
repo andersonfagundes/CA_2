da84420eb2f503cdb0773fed0cda640499559c09
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-04-30 11:48:38 +0100

    Fix menu options to reflect correct functionality

--- TechCompanyApp/src/techcompanyapp/MenuOption.java
+++ TechCompanyApp/src/techcompanyapp/MenuOption.java
@@ -1,9 +1,8 @@
 package techcompanyapp;
 
 public enum MenuOption {
-    ADD_EMPLOYEE,
-    GENERATE_RANDOM_EMPLOYEE,
-    SORT_EMPLOYEES,
-    SEARCH_EMPLOYEE,
+    SORT,
+    SEARCH,
+    ADD_RECORDS,
     EXIT
 }
