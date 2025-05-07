8ab491db98ab5df74d0d5e21e52f147dcf57d73e
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-05-07 16:31:02 +0100

    Add comments to DepartmentMenuOption class

--- src/CA_2/DepartmentMenuOption.java
+++ src/CA_2/DepartmentMenuOption.java
@@ -1,7 +1,16 @@
+// This line says the enum is part of the CA_2 package
 package CA_2;
 
+// This is an enum (a list of fixed options) called DepartmentMenuOption
+// It represents the different departments in the company
 public enum DepartmentMenuOption {
+
+    // Option for Customer Service department
     CUSTOMER_SERVICE,
+
+    // Option for Technical Support department
     TECHNICAL_SUPPORT,
-    HR   
+
+    // Option for Human Resources (HR) department
+    HR
 }
