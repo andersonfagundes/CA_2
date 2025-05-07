586f39b07c9a4b982e837efbcd6a76e1e87d140b
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-05-07 16:27:08 +0100

    Add comments to MenuOption class

--- src/CA_2/MenuOption.java
+++ src/CA_2/MenuOption.java
@@ -1,8 +1,18 @@
+// This line says the enum is part of the CA_2 package
 package CA_2;
 
+// This is an enum (a list of fixed options) called MenuOption
 public enum MenuOption {
+    
+    // Option to sort the list of employees
     SORT,
+
+    // Option to search for an employee
     SEARCH,
+
+    // Option to add new employee records
     ADD_RECORDS,
+
+    // Option to exit the program
     EXIT
 }
