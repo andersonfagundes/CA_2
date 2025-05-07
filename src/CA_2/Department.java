1461d78bb635f9e6b71233c17e8397f7e67b9a81
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-05-07 16:22:18 +0100

    Add comments to Department class

--- src/CA_2/Department.java
+++ src/CA_2/Department.java
@@ -1,12 +1,18 @@
+// This line defines the package where this class is located
 package CA_2;
 
+// This defines a class called Department
 public class Department {
+
+    // This variable stores the name of the department
     private String name;
 
+    // This is the constructor. It sets the department name when the object is created.
     public Department(String name) {
         this.name = name;
     }
 
+    // This method returns the name of the department
     public String getName() {
         return name;
     }
