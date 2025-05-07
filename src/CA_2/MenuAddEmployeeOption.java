6642bc16b87b0db62d5e372f5665a17adf185d6b
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-05-07 16:28:43 +0100

    Add comments to MenuAddEmployeeOption class

--- src/CA_2/MenuAddEmployeeOption.java
+++ src/CA_2/MenuAddEmployeeOption.java
@@ -1,6 +1,12 @@
+// This line says the enum is part of the CA_2 package
 package CA_2;
 
+// This is an enum (a list of options) called MenuAddEmployeeOption
 public enum MenuAddEmployeeOption {
+
+    // Option to add an employee manually (user types the info)
     ADD_EMPLOYEE,
+
+    // Option to add an employee with random (automatic) data
     GENERATE_RANDOM_EMPLOYEE
 }
